9eca9db60938292806b531920d7129dea0ea66c9
Maurílio <mauriliommachado@gmail.com>
2017-10-20 02:37:49 -0200

    a

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,7 @@ export class HomePage {
   public elo: string;
   public wr: string;
   public id: number;
+  public checkStatus: boolean = true;
   public user;
 
   constructor(public navCtrl: NavController,
@@ -25,7 +26,7 @@ export class HomePage {
 
   doGet() {
     let loader = this.loadingCtrl.create({
-      content: "Criando usuário, aguarde...",
+      content: "Buscando usuários, aguarde...",
     });
     loader.present();
     let header: Headers = new Headers();
@@ -40,21 +41,24 @@ export class HomePage {
           this.elo = JSON.parse(data)[0].name;
           this.wr = JSON.parse(data)[0].id;
           this.id = JSON.parse(data)[0].id;
+          if (!this.id == null)  {
+            this.checkStatus = true;
+            return;
+          }
+          this.checkStatus = false;
         }
       }, err => {
         console.log(err);
-
         loader.dismiss();
-
-        this.alertCtrl.create({
-          title: 'Falha no cadastro',
-          buttons: [{ text: 'Ok' }],
-          subTitle: 'Não foi possível fazer o cadastro com os dados enviados.'
-        }).present();
+        this.checkStatus = true;
       })
   }
 
   match() {
+    this.doCall(true);
+  }
+
+  doCall(statusMatch: boolean) {
     let loader = this.loadingCtrl.create({
       content: "Aguarde...",
     });
@@ -63,26 +67,38 @@ export class HomePage {
     header.append('Content-Type', 'application/json');
     header.append('Authorization', 'Basic ZGlpYW5rOmFkbWlu');
     let options = new RequestOptions({ headers: header });
-    var match = { id: this.user.id, matchId: this.id, status: true };
+    var match = { id: this.user.id, matchId: this.id, status: statusMatch };
     this.http.post('http://localhost:8080/api/match', match, options)
       .map(res => res.text())
       .toPromise().then(data => {
         loader.dismiss();
-        this.doGet()
+        if(JSON.parse(data).status){
+          this.alertCtrl.create({
+            title: 'Duo!!',
+            buttons: [{ text: 'Ok' }],
+            subTitle: 'Entre em contato com seu novo Duo!'
+          }).present();
+        }
+        this.checkStatus = true;
         this.elo = null;
         this.wr = null;
         this.id = null;
+        this.doGet()
       }, err => {
         console.log(err);
-
+        this.checkStatus = true;
         loader.dismiss();
 
         this.alertCtrl.create({
-          title: 'Falha no cadastro',
+          title: 'Falha na chamada',
           buttons: [{ text: 'Ok' }],
           subTitle: 'Não foi possível fazer o cadastro com os dados enviados.'
         }).present();
       })
+  }
+
+  notMatch() {
+    this.doCall(false);
   }
 
 }
